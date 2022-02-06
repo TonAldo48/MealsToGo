66c14287e51cd3ab9bcc0a0746c43365cdabdf63
David Nintang Junior <nintang48@gmail.com>
2022-02-06 16:39:15 +0000

    Built the spacer component

---
+++ src/components/spacer/spacer.component.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components/native';
+
+const sizeVariant = {
+    small: 1,
+    medium: 2,
+    large: 3,
+}
+
+const positionVariant = {
+    top: 'marginTop',
+    left: 'marginLeft',
+    right: 'marginRight',
+    bottom: 'marginBottom', 
+}
+
+const getVariant = (position, size, theme) => {
+    const sizeIndex = sizeVariant[size];
+    const property = positionVariant[position];
+    const value = theme.space[sizeIndex];
+
+    return `${property}:${value}`;
+}
+
+export const Spacer = styled(View)`
+
+    ${({position, size}) => getVariant(position, size)}
+`;
+
+Spacer.defaultProps = {
+    position: 'top',
+    size: 'small'
+}
